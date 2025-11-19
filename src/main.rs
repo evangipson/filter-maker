2382bc6d52237ef1e4039ae71c7ecebfcd46820f
Evan Gipson <evangipson@gmail.com>
2025-11-18 18:33:13 -0600

    Clean up script and README

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
-use filter_maker::{
-    config::filter::Filter,
-    constants::filter,
-    os::{copy_file},
-};
+use filter_maker::{config::filter::Filter, constants::filter, os::copy_file};
 use std::env;
 use std::fs;
 use std::io::Error;
@@ -29,9 +25,6 @@ fn main() -> Result<(), Error> {
     if !destination_path.is_empty() {
         copy_file::copy_filter(destination_path);
         Ok(())
-    // } else if let Ok(filter_destination) = find_directory::find_filter_destination() {
-    //     copy_file::copy_filter(filter_destination.as_path().to_str().unwrap());
-    //     Ok(())
     } else {
         Err(Error::new(
             std::io::ErrorKind::NotFound,
--- update.ps1
+++ update.ps1
@@ -4,38 +4,23 @@
     
     .DESCRIPTION
         Runs the underlying filter-maker rust program, which uses a TOML configuration file
-        to write a new item filter for Path of Exile 1 or 2. A;so makes sure rust is installed
+        to write a new item filter for Path of Exile 1 or 2. Also makes sure rust is installed
         before attempting to run the filter-maker program.
+
+    .PARAMETER Poe2
+        An optional switch that will build the new item filter and then copy it to the Path
+        of Exile 2 installation folder instead.
     
     .PARAMETER Filter
-        A relative path to the TOML configuration file.
-
-    .PARAMETER Destination
-        An optional absolute path for the destination of the generated filter. If it is not
-        provided, the filter-maker application will do it's best to find where Path of Exile
-        is installed and copy the filter there for you.
-
-    .EXAMPLE
-        PS> ./update.ps1 -Filter ./config/filter.toml
-
-        Looks for a TOML configuration file named "filter.toml" in the config/ directory, and
-        copies the new item filter to the default Path of Exile installation directory.
-
-        PS> ./update.ps1 -Filter ./config/filter.poe1.toml -Destination "C:/Path of Exile"
-
-        Looks for a TOML configuration file named "filter.poe1.toml" in the config/ directory,
-        and copies the new item filter to the "C:/Path of Exile" directory.
+        An optional relative path to a custom TOML configuration file.
 #>
 
 param(
-    [Parameter(Mandatory=$false)]
-    [switch] $poe2,
-    [string] $Filter,
-    [string] $Destination
+    [switch] $Poe2,
+    [string] $Filter
 )
 
 function Test-RustInstallation {
-    
     try {
         rustc --version
         return $true
@@ -51,25 +36,28 @@ function Get-LatestCode {
     return $LASTEXITCODE -eq 0
 }
 
-function Write-NewFilter([string] $FilterPath, [string] $DestinationPath) {
-    Write-Host "Generating new filter..." -ForegroundColor DarkGray
-    if (-not $poe2) {
-        $FilterPath = "config/filter.poe1.toml"
-        $DestinationPath = (Get-ChildItem -Path ([Environment]::GetFolderPath([Environment+SpecialFolder]::MyDocuments)) -Directory -Recurse -Filter "Path of Exile" -ErrorAction SilentlyContinue).FullName        
+function Get-FilterPath([string] $FilterPath) {
+    # allow a user to define their own filter path on the script
+    if (![string]::IsNullOrWhiteSpace($FilterPath)) {
+        return $FilterPath
     }
-    else {
-        $FilterPath = "config/filter.poe2.toml"
-        $DestinationPath = (Get-ChildItem -Path ([Environment]::GetFolderPath([Environment+SpecialFolder]::MyDocuments)) -Directory -Recurse -Filter "Path of Exile 2" -ErrorAction SilentlyContinue).FullName     
-    }
-    if ([string]::IsNullOrWhiteSpace($DestinationPath)) {
-        cargo run -- $FilterPath
-    }else{
-        cargo run -- $FilterPath $DestinationPath
-    return $LASTEXITCODE -eq 0
-    }
+
+    # otherwise infer the filter path based on the -Poe2 switch
+    return $Poe2 ? "config/filter.poe2.toml" : "config/filter.poe1.toml"
 }
 
-function Update-Filter([string] $FilterPath, [string] $DestinationPath) {
+function Get-DestinationPath {
+    $poeInstallDir = $Poe2 ? "Path of Exile 2" : "Path of Exile"
+    return (Get-ChildItem -Path ([Environment]::GetFolderPath([Environment+SpecialFolder]::MyDocuments)) -Directory -Recurse -Filter $poeInstallDir -ErrorAction SilentlyContinue).FullName
+}
+
+function Write-NewFilter([string] $FilterPath) {
+    Write-Host "Generating new filter..." -ForegroundColor DarkGray
+    cargo run -- (Get-FilterPath -FilterPath $FilterPath) (Get-DestinationPath)
+    return $LASTEXITCODE -eq 0
+}
+
+function Update-Filter([string] $FilterPath) {
     # Make sure rust is installed
     if ($false -eq (Test-RustInstallation)) {
         Write-Host "Please install rust to generate your filter by visiting https://www.rust-lang.org/tools/install." -ForegroundColor White
@@ -83,7 +71,7 @@ function Update-Filter([string] $FilterPath, [string] $DestinationPath) {
     }
 
     # Generate the filter
-    if ($false -eq (Write-NewFilter -FilterPath $FilterPath -DestinationPath $DestinationPath)) {
+    if ($false -eq (Write-NewFilter -FilterPath $FilterPath)) {
         Write-Host "There was an issue generating the new filter." -ForegroundColor DarkRed
         exit 0
     }
@@ -91,4 +79,4 @@ function Update-Filter([string] $FilterPath, [string] $DestinationPath) {
     Write-Host "✓ All done!"
 }
 
-Update-Filter -FilterPath $Filter -DestinationPath $Destination
+Update-Filter -FilterPath $Filter
